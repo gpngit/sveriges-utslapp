090c518804d7d1ede7db8a84fc3501507d050653
Vidar Rehn <vidar.rehn@gmail.com>
2022-11-08 14:14:34 +0100

    added Hero component and imported to index

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import initFirebase from '../firebase/initFirebase'
 import { getDatabase, ref, child, get } from "firebase/database"
 //SCB
 import { getDataFromScbAndTransferToFirebase } from '../scb/fetch'
+//components
+import Hero from '../components/hero/Hero'
 
 
 export async function getServerSideProps(){
@@ -43,6 +45,7 @@ export default function Home({ sections, bioEmissions }) {
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
     </Head>
+    <Hero />
     </>
   )
 }
