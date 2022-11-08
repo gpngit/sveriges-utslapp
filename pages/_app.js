77edac9c97388370c509de160f39e810d9411034
Vidar Rehn <vidar.rehn@gmail.com>
2022-11-08 16:45:32 +0100

    added react context for global state handling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+//CSS
 import '../styles/globals.css'
+//context
+import AppContext from '../context/AppContext'
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [authenticated, setAuthenticated] = useState(false)
+
+  return (
+    <AppContext.Provider value={{ authenticated, setAuthenticated }}>
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  )
 }
 
 export default MyApp
--- pages/admin.js
+++ pages/admin.js
@@ -1,5 +1,12 @@
+//CSS
+//context
+import { useContext } from 'react'
+import AppContext from '../context/AppContext'
 
 export default function Admin() {
+
+    const context = useContext(AppContext)
+    const {authenticated, setAuthenticated} = context
 
     return (
         <>Adminportal</>
