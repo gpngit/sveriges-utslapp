66002540afc797ec3225b61d0250cdeef1800703
Vidar Rehn <vidar.rehn@gmail.com>
2022-11-08 15:00:46 +0100

    styling on heropage

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -2,12 +2,29 @@ import styled, {css} from 'styled-components'
 
 const Container = styled.header`
     min-height: 100vh;
+    padding: 30px;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+    background-color: #f2cebd; //primaryLight
+    color: #540707; // primaryDark
 `
 const Crumble = styled.div`
-
+    align-self: center;
 `
 const TextContent = styled.div`
+    width: 70%;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
 
+    h1 {
+        font-size: 36px;
+    }
+
+    p {
+        font-size: 24px;
+    }
 `
 
 const Hero = ({ sectionDetails }) => {
@@ -16,7 +33,9 @@ const Hero = ({ sectionDetails }) => {
 
     return (
         <Container>
-            <Crumble></Crumble>
+            <Crumble>
+                <p>En sammanställning av <span>GREENPEACE</span></p>
+            </Crumble>
             <TextContent>
                 <p>{subheading.heading}</p>
                 <h1>{title.heading}</h1>
