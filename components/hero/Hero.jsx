1cc26ec16f667ea4c6b404148325321919112676
Vidar Rehn <vidar.rehn@gmail.com>
2022-11-08 14:41:22 +0100

    started to render out text content on heropage

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -1,8 +1,32 @@
+import styled, {css} from 'styled-components'
 
-const Hero = () => {
+const Container = styled.header`
+    min-height: 100vh;
+`
+const Crumble = styled.div`
+
+`
+const TextContent = styled.div`
+
+`
+
+const Hero = ({ sectionDetails }) => {
+
+    const {subheading, title, articles} = sectionDetails
 
     return (
-        <></>
+        <Container>
+            <Crumble></Crumble>
+            <TextContent>
+                <p>{subheading.heading}</p>
+                <h1>{title.heading}</h1>
+                {articles.map(article => {
+                    return (
+                        <p>{article.article}</p>
+                    )
+                })}
+            </TextContent>
+        </Container>
     )
 }
 
