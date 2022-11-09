b0ac8a1c8948948f9ad158e19e55842665bc3200
Vidar Rehn <vidar.rehn@gmail.com>
2022-11-09 15:17:36 +0100

    show save and discard buttons when editable

--- components/admin/InputContainer.jsx
+++ components/admin/InputContainer.jsx
@@ -36,8 +36,13 @@ const InputContainer = ({ name, input, label }) => {
             <Label htmlFor={`${name}-title`}>{label}</Label>
             <div className="input-and-edit">
                 <Input readOnly={!editable} id={`${name}-title`} type="text" defaultValue={input.text} />
-                {!editable && (
+                {!editable ? (
                     <button onClick={(e) => handleEditClick(e)}>Edit</button>
+                ) : (
+                    <>
+                    <button>Ångra ändring</button>
+                    <button>Spara</button>
+                    </>
                 )}
             </div>
         </Container>
