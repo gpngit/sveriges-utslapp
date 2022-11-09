2d634a5713d1dc867f5f2fdf5dee202ed5f2262f
Vidar Rehn <vidar.rehn@gmail.com>
2022-11-09 10:07:01 +0100

    getting firebase data at Portal

--- components/admin/Portal.jsx
+++ components/admin/Portal.jsx
@@ -1,5 +1,10 @@
 //CSS
 import styled from 'styled-components'
+//react hooks
+import { useEffect, useState } from 'react'
+//firebase
+import initFirebase from '../../firebase/initFirebase'
+import { getDatabase, ref, set, child, push, update, onValue } from "firebase/database"
 
 const Container = styled.main`
     min-height: 100vh;
@@ -7,8 +12,37 @@ const Container = styled.main`
 
 const Portal = () => {
 
+    const [pages, setPages] = useState(null)
+    const [heropage, setHeroPage] = useState(null)
+
+    const getDataFromFirebase = (route) => {
+        const db = getDatabase()
+        const dbRef = ref(db, route)
+        onValue(dbRef, (snapshot) => {
+          setPages(snapshot.val().pages)
+          setHeroPage(snapshot.val().heropage)
+        })
+    }
+
+    useEffect(() => {
+        initFirebase()
+        getDataFromFirebase('admin/')
+    }, [])
+
+    console.log(pages)
+    console.log(heropage)
+
     return (
-        <Container>Portal</Container>
+        <Container>
+            <h2>Adminportal</h2>
+            {heropage && heropage.map(page => {
+                console.log(page)
+                return (
+                    <form key={page.pageId}>
+                    </form>
+                )
+            })}
+        </Container>
     )
 }
 
