bcea777f2a2d00a4164045b24927ef42b7ebd69e
Tova Jertfelt <tovizitan@gmail.com>
2022-11-14 09:49:49 +0100

    added media queries as a mixins in partials.js, added font as exportable const in partials.js

--- styles/partials.js
+++ styles/partials.js
@@ -17,6 +17,9 @@ export const fontSizes = {
     paragraph: '1.6em'
 }
 
+export const font ={
+    main: "Noto Sans, sans-serif"
+}
 
 // mixins
 
@@ -26,3 +29,23 @@ export const flex = (direction, justify, align) => css`
     justify-content: ${justify || 'flex-start'};
     align-items: ${align || 'flex-start'};
 `
+const size = {
+    mobileS: '320px',
+    mobileM: '375px',
+    mobileL: '425px',
+    tablet: '768px',
+    laptop: '1024px',
+    laptopL: '1440px',
+    desktop: '2560px'
+}
+export const device = {
+    mobileS: `(min-width: ${size.mobileS})`,
+    mobileM: `(min-width: ${size.mobileM})`,
+    mobileL: `(min-width: ${size.mobileL})`,
+    tablet: `(min-width: ${size.tablet})`,
+    laptop: `(min-width: ${size.laptop})`,
+    laptopL: `(min-width: ${size.laptopL})`,
+    desktop: `(min-width: ${size.desktop})`,
+    desktopL: `(min-width: ${size.desktop})`
+  };
+
