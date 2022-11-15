bf429d253635e2265ef16aa8ffca6aac45a87df0
Tova Jertfelt <tovizitan@gmail.com>
2022-11-15 15:29:30 +0100

    flyttat tillbaka Linechart

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ import FaktaTwo from './sections/yearlyFacts/factsTwo/faktaTwo'
 import SectionTypeOne from './sections/sectionDifferentTypes/SectionOnePicture'
 import SectionTypeTwo from './sections/sectionDifferentTypes/SectionTwoPictures'
 import SectionTypeThree from './sections/sectionDifferentTypes/SectionNoPicture'
-import Tidslinjen from './sections/tidslinje/Tidslinje'
+import LineChart from '../components/lineChart/LineChart'
 
 export async function getServerSideProps(){
   initFirebase()
@@ -72,7 +72,7 @@ export default function Home({ siteSections, emissions }) {
     <HeadContent/>
     <Hero pageElements={siteSections.find(elem => elem.name === 'hero')} />
     <Ingress pageElements={siteSections.find(elem => elem.name === 'ingress')}  />
-    <Tidslinjen emissions={emissions}/>
+    <LineChart emissions={emissions}/>
     <YearChanger emissions={emissions} />
     <Slider emissions={emissions}/>
     <FaktaOne pageElements={siteSections.find(elem => elem.name === 'faktaOne')}
