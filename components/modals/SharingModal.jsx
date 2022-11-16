76ce614f58d93306b0bb074556d68f330df22d65
Tova Jertfelt <tovizitan@gmail.com>
2022-11-16 16:28:55 +0100

    basfunktioner

--- components/modals/SharingModal.jsx
+++ components/modals/SharingModal.jsx
@@ -1,5 +1,6 @@
 import { flex, device, fonts, colors } from "../../styles/partials";
 import styled, {css} from "styled-components";
+import Link from "next/link";
 
 const Container= styled.div`
 margin:0;
@@ -7,18 +8,40 @@ background-color: ${colors.primary}
 border-radius:19px
 z-index:20;
 `
-const Content=styled.span`
+const Content=styled.div`
 ul{
   list-type:none;
 }
 `
 const SharingModal = () => {
+  const copyURL = () => {
+    
+  }
+  const closeModal=() => {
+  console.log("Stäng")
+  }
+
   return ( 
   <Container>
-    <div className="close">X</div>
+    <div className="close"
+    onClick={closeModal}>X</div>
     <Content>
     <p>Dela detta via</p>
-    <ul></ul>
+    <ul>
+    <Link href="#">Facebook</Link>
+    <Link href="#">Instagram</Link>
+    <Link href="#">Twitter</Link>
+    <Link href="#">Whatsapp</Link>
+    </ul>
+    <p>Eller kopiera länken:</p>
+    <span className="field">
+      <input 
+      id="inputShareLink"
+      type="text"
+      readOnly 
+      value="example.com/share-link"/>
+      <button onClick={copyURL}>Kopiera</button>
+    </span>
     </Content>
   </Container>
   );
