3dc4a07e95072719bb07869c51bb75982f32d93d
Vidar Rehn <vidar.rehn@gmail.com>
2022-11-16 09:57:09 +0100

    fixed so that 1989 and 2021/latestyear are not clickable

--- components/year-changer/YearChanger.jsx
+++ components/year-changer/YearChanger.jsx
@@ -1,6 +1,6 @@
 //CSS
 import styled from 'styled-components'
-import { flex, colors} from '../../styles/partials'
+import { flex, colors, fonts } from '../../styles/partials'
 //react hooks
 import { useState, useEffect } from 'react'
 //context
@@ -12,33 +12,38 @@ import Chevron from '../../public/Chevron'
 import Image from "next/image";
 
 const Container = styled.div`
+    ${fonts.footnote};
     z-index: 10;
     width: 100%;
     position: sticky;
     top: 0;
     ${flex('row', 'space-around', 'center')};
-    padding: 20px;
     background-color: ${colors.primary};
+    padding: 10px;
 `
 const InnerContainer = styled.div`
-    ${flex('row', 'space-around', 'center')};
-    gap: 30px;
+    ${flex('row', 'space-between', 'center')};
 
     div {
-        ${flex('row', 'space-around', 'center')};
-        gap: 10px;
+        ${flex('row', 'space-between', 'center')};
+    }
+
+    .inactive {
+        text-decoration: line-through;
     }
 `
 const Year = styled.span`
-    font-size: 30px;
+    ${fonts.paragraph};
     font-weight: bold;
+    padding: 10px;
 `
 const Button = styled.button`
     background-color: ${colors.primary};
+    ${fonts.footnote}
     color: ${colors.secondary};
     border: 3px solid ${colors.secondary};
     border-radius: 10px;
-    padding: 10px 30px;
+    padding: 10px 20px;
     height: 50px;
 `
 
@@ -49,6 +54,8 @@ const YearChanger = ({ emissions }) => {
     const [years, setYears] = useState([... new Set(emissions.map(emission => emission.year))])
     const latestYear = Number(years[years.length-1])
     const firstYear = Number(years[0])
+    const [reachedBeginning, setReachedBeginning] = useState(true)
+    const [reachedEnd, setReachedEnd] = useState(false)
 
     const increment = () => {
         if (displayYear != latestYear){
@@ -62,18 +69,23 @@ const YearChanger = ({ emissions }) => {
         }
     }
 
+    useEffect(() => {
+        displayYear == firstYear ? setReachedBeginning(true) : setReachedBeginning(false)
+        displayYear == latestYear ? setReachedEnd(true) : setReachedEnd(false)
+    }, [displayYear])
+
     return (
         <Container>
             <Button onClick={() => setDisplayYear(firstYear)}>{firstYear}</Button>
             <InnerContainer>
                 <div onClick={() => decrement()}>
-                    <Chevron color={colors.secondary} size={30} direction={'left'} />
-                    <p>{displayYear-1}</p>
+                    <Chevron color={colors.secondary} size={30} direction={'left'} stroke={5} />
+                    <p className={reachedBeginning && 'inactive'}>{displayYear-1}</p>
                 </div>
                 <Year>{displayYear}</Year>
                 <div onClick={() => increment()}>
-                    <p>{displayYear+1}</p>
-                    <Chevron color={colors.secondary} size={30} direction={'right'} />
+                    <p className={reachedEnd && 'inactive'}>{displayYear+1}</p>
+                    <Chevron color={colors.secondary} size={30} direction={'right'} stroke={5}  />
                 </div>
             </InnerContainer>
             <Button onClick={() => setDisplayYear(latestYear)}>{latestYear}</Button>
--- public/Chevron.jsx
+++ public/Chevron.jsx
@@ -1,22 +1,22 @@
 
-const Chevron = ({ color, size, direction }) => {
+const Chevron = ({ color, size, direction, stroke }) => {
     return (
         <>
         {direction === 'left' ? 
         <svg height={size} viewBox="0 0 37 66" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M34 3L4 33L34 63" stroke={color} stroke-width="5" stroke-linecap="round"/>
+            <path d="M34 3L4 33L34 63" stroke={color} strokeWidth={stroke} strokeLinecap="round"/>
         </svg>
         : direction === 'right' ?
         <svg height={size} viewBox="0 0 37 66" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M3 3L33 33L3 63" stroke={color} stroke-width="5" stroke-linecap="round"/>
+            <path d="M3 3L33 33L3 63" stroke={color} strokeWidth={stroke} strokeLinecap="round"/>
         </svg>
         : direction === 'down' ?
         <svg width={size} viewBox="0 0 66 37" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M63 3L33 33L3 3" stroke={color} stroke-width="5" stroke-linecap="round"/>
+            <path d="M63 3L33 33L3 3" stroke={color} strokeWidth={stroke} strokeLinecap="round"/>
         </svg>
         :
         <svg width={size} viewBox="0 0 66 37" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M3 34L33 4L63 34" stroke={color} stroke-width="5" stroke-linecap="round"/>
+            <path d="M3 34L33 4L63 34" stroke={color} strokeWidth={stroke} strokeLinecap="round"/>
         </svg>
         }
         </>
