f6cf7950c0d00ae0bacc701c49cdcbb59e852566
Tova Jertfelt <tovizitan@gmail.com>
2022-11-16 16:41:08 +0100

    pausar för idag

--- components/buttons/SourceAndShare.jsx
+++ components/buttons/SourceAndShare.jsx
@@ -2,6 +2,7 @@
 import styled, {css} from 'styled-components'
 import { flex, colors, fonts  } from '../../styles/partials'
 import { useEffect, useState } from 'react'
+import SharingModal from '../modals/SharingModal'
 
 const Container = styled.div`
     ${flex('row')};
@@ -31,10 +32,34 @@ const LinkButton = styled.a`
         color:${colors.secondary};
         `}
 `
+const Button = styled.button`
+${flex('row', 'center', 'center')}
+background-color: transparent;
+text-decoration: none;
+color: white;
+border: 3px solid white;
+border-radius: 10px;
+width: 100px;
+height: 40px;
+${fonts.footnote};
+
+${props => 
+    props.secondary && 
+    css`
+    background-color: transparent;
+    text-decoration: none;
+    border-radius: 10px;
+    width: 100px;
+    height: 40px;
+    border: 3px solid ${colors.secondary};
+    color:${colors.secondary};
+    `}
+`
 
 const SourceAndShare = ({ whiteBG, sourceLink, shareLink }) => {
     const [showSecondary, setShowSecondary] = useState(false);
     console.log(shareLink, "shareLink")
+    const [showModal, setShowModal] = useState(false);
     
     useEffect(() => {
         if(whiteBG === "yes"){
@@ -42,19 +67,34 @@ const SourceAndShare = ({ whiteBG, sourceLink, shareLink }) => {
         }
     },[whiteBG])
 
+    const shareModal= () => {
+        setShowModal(true)
+    }
+
     return (
         <Container>
             {showSecondary ? (<>
                 <LinkButton  secondary
                     href={sourceLink}>Källa</LinkButton>
-                <LinkButton secondary 
-                    href={shareLink}>Dela</LinkButton></>
+                <Button secondary
+                source={shareLink}
+                onClick={shareModal}>Dela</Button>
+                {/* <LinkButton secondary 
+                    href={shareLink}>Dela</LinkButton> */}
+                    </>
             ): ( <>
                 <LinkButton
                     href={sourceLink}>Källa</LinkButton>
-                <LinkButton 
-                    href={shareLink}>Dela</LinkButton></>
+                <Button 
+                source={shareLink}
+                onClick={shareModal}>Dela</Button>
+                {/* <LinkButton 
+                    href={shareLink}>Dela</LinkButton> */}
+                    </>
+               
             )}
+            {showModal ? (<SharingModal
+            source={shareLink} />) : (null)}
            
         </Container>
     )
--- components/modals/SharingModal.jsx
+++ components/modals/SharingModal.jsx
@@ -13,6 +13,9 @@ ul{
   list-type:none;
 }
 `
+const ShareButton=styled.div`
+`
+
 const SharingModal = () => {
   const copyURL = () => {
     
@@ -23,12 +26,23 @@ const SharingModal = () => {
 
   return ( 
   <Container>
-    <div className="close"
-    onClick={closeModal}>X</div>
     <Content>
     <p>Dela detta via</p>
     <ul>
-    <Link href="#">Facebook</Link>
+    <Link 
+    href="https://facebook.com/sharer/sharer.php?u=http%3A%2F%2Fsharingbuttons.io" 
+    target="_blank" 
+    rel="noopener" 
+    aria-label="Share on Facebook">
+      <ShareButton>
+        <div aria-hidden="true">
+          <svg xmlns="http://www.w3.org/2000/svg" 
+          viewBox="0 0 24 24">
+            <path d="M18.77 7.46H14.5v-1.9c0-.9.6-1.1 1-1.1h3V.5h-4.33C10.24.5 9.5 3.44 9.5 5.32v2.15h-3v4h3v12h5v-12h3.85l.42-4z"/>
+          </svg>
+        </div>Dela på Facebook
+      </ShareButton>
+    </Link>
     <Link href="#">Instagram</Link>
     <Link href="#">Twitter</Link>
     <Link href="#">Whatsapp</Link>
@@ -38,13 +52,17 @@ const SharingModal = () => {
       <input 
       id="inputShareLink"
       type="text"
+      aria-label="read only input"
       readOnly 
       value="example.com/share-link"/>
-      <button onClick={copyURL}>Kopiera</button>
+      <button 
+      aria-label="Kopiera länken"
+      onClick={copyURL}>Kopiera</button>
     </span>
     </Content>
   </Container>
   );
 }
- 
 export default SharingModal;
+
+// https://sharingbuttons.io/
