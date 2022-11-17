74d4c34a1410cd76d14efb7e453b728c1eaa3d54
Tova Jertfelt <tovizitan@gmail.com>
2022-11-17 11:32:41 +0100

    fixed bug in footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -2,31 +2,12 @@
 import styled from 'styled-components'
 import { flex, colors, font } from '../../styles/partials'
 //nextjs components
-import {
-    FacebookShareCount,
-    HatenaShareCount,
-    OKShareCount,
-    PinterestShareCount,
-    TumblrShareCount,
-    VKShareCount,
-  } from 'next-share';
+
 import Link from 'next/link'
 //components
 import Crumble from '../crumble/Crumble'
 
 
-{/* <FacebookShareCount
-url={'https://github.com/next-share'}
-appId={''}
-appSecret={''}
-/> */}
-{/* <FacebookShareCount
-  url={'https://github.com/next-share'}
-  appId={''}
-  appSecret={''}
->
-  {shareCount => <span className="wrapper">{shareCount}</span>}
-</FacebookShareCount> */}
 
 const Container = styled.footer`
     padding: 30px;
@@ -61,8 +42,8 @@ const Footer = ({ pageElements }) => {
                 <Subheading>{subheading.text.toUpperCase()}</Subheading>
                 <Title>{title.text}</Title>
                 <LinksContainer>
-                    {links.map(link => 
-                    <Link key={link.link}
+                    {links.map((link,indx) => 
+                    <Link key={indx}
                     href={link.link}>
                         {link.name}</Link>)}
                 </LinksContainer>
