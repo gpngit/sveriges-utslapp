3c8dd8734f2c6f5e4113b7828782c1bf342a3471
Vidar Rehn <vidar.rehn@gmail.com>
2022-11-21 11:02:15 +0100

    added Google tag manager script in Head

--- components/Head.js
+++ components/Head.js
@@ -1,12 +1,30 @@
 import Head from "next/head";
+import Script from 'next/script'
 
 const HeadContent = () => {
   return ( 
+    <>
     <Head>
       <title>Sveriges utsläpp</title>
       <meta name="description" content="En hemsida om de dolda uptsläppen." />
       <link rel="icon" href="/favicon.ico" />
     </Head>
+    <Script 
+      id='gtag'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NVWTHN6');
+            `
+      }}
+    />
+    </>
    );
 }
  
