96edb0867b910c7a653a63b3bf00176864919fcd
Tova Jertfelt <tovizitan@gmail.com>
2022-11-23 14:27:18 +0100

    added console log

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,6 @@ import Ingress from '../components/sections/ingress/Ingress'
 import LineChart from '../components/lineChart/LineChart'
 import FaktaPages from '../components/sections/yearlyFacts/FaktaPages'
 import SectionOne from '../components/sections/sectionDifferentTypes/SectionOne'
-import SectionTypeTwo from "../components/sections/sectionDifferentTypes/SectionTwoPictures"
-import Section from '../components/sections/sectionDifferentTypes/Section'
 import SectionTwo from '../components/sections/sectionDifferentTypes/SectionTwo'
 import SectionThree from '../components/sections/sectionDifferentTypes/SectionThree'
 
@@ -45,7 +43,10 @@ export async function getServerSideProps(){
 }
 
 export default function Home({ siteSections, emissions }) {
-console.log(siteSections)
+
+const showSections = siteSections.filter(item => item.show);
+console.log(showSections, "array")
+
   useEffect(() => {
     window.addEventListener('scroll', () => {
       let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight)
