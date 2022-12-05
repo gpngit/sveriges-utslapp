daf073c266b66bcd51181c099bd1be40f52cb41b
Tova Jertfelt <tovizitan@gmail.com>
2022-12-05 13:13:24 +0100

    accessibility fixed on crumble

--- components/crumble/Crumble.jsx
+++ components/crumble/Crumble.jsx
@@ -19,14 +19,15 @@ const Container = styled.div`
 `
 
 const Crumble = ({ color }) => {
-
     return (
         <Container>
             <p>En sammanställning av</p>
             <Link href={"https://www.greenpeace.org/sweden/"} 
             target="_blank"
             aria-label='Link to Greenpeace'>
-            <GreenpeaceLogo alt='Greenpeace-logo' 
+            <GreenpeaceLogo 
+            role="img"
+            alt='Greenpeace:s logotyp' 
             color={color} />
             </Link>
         </Container>
